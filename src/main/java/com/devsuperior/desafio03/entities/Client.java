efcbda3b4374662a9032e45f9ec17d29f2cf1490
Rafael <rafaelvpds.dev09@gmail.com>
2024-01-02 17:40:08 -0300

    update/ entitie class atribute id

--- src/main/java/com/devsuperior/desafio03/entities/Client.java
+++ src/main/java/com/devsuperior/desafio03/entities/Client.java
@@ -3,12 +3,16 @@ package com.devsuperior.desafio03.entities;
 import java.time.LocalDateTime;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_clients")
 public class Client {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String cpf;
