ad2e43f5e8c05e980cfbabe68d66ab9a8a53b391
Rafael <rafaelvpds.dev09@gmail.com>
2024-01-02 17:39:06 -0300

    Create entitie client

---
+++ src/main/java/com/devsuperior/desafio03/entities/Client.java
@@ -0,0 +1,79 @@
+package com.devsuperior.desafio03.entities;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_clients")
+public class Client {
+
+    private Long id;
+    private String name;
+    private String cpf;
+    private Double income;
+    private LocalDateTime birthDate;
+    private Integer children;
+
+    public Client() {
+    }
+
+    public Client(Long id, String name, String cpf, Double income, LocalDateTime birthDate, Integer children) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.income = income;
+        this.birthDate = birthDate;
+        this.children = children;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Double getIncome() {
+        return income;
+    }
+
+    public void setIncome(Double income) {
+        this.income = income;
+    }
+
+    public LocalDateTime getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDateTime birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Integer getChildren() {
+        return children;
+    }
+
+    public void setChildren(Integer children) {
+        this.children = children;
+    }
+
+}
